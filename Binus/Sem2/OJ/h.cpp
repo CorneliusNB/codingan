0bc6474d463b383d74359e04ebdd08fa89d98cf0
CorneliusNB <tanel2112@gmail.com>
2026-06-08 10:15:20 +0700

    new stuff

---
+++ Binus/Sem2/OJ/h.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct HASH{
+  char key;
+  int value;
+  bool kepake;
+};
+
+HASH hashTable[5];
+
+int hashMyShit(char val){
+  return val % 5;
+}
+
+void insert(char val){
+    
+}
+
+
+
+
+int main(){
+  
+}
